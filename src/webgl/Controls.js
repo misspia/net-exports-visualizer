229a69c32ff3b5b88287c61a68184f702da17991
misspia <pia_leung@hotmail.com>
2021-01-25 00:47:26 +0000

    set min/max camera distance

--- src/webgl/Controls.js
+++ src/webgl/Controls.js
@@ -3,5 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 export default class Controls extends OrbitControls {
   constructor(context) {
     super(context.camera, context.renderer.domElement);
+    this.minDistance = 3;
+    this.maxDistance = 20; 
   }
 }
